c16cb20ef06e51da54b8388ad4325f6c5cdaaafc
Jared Cazel <jjcazel@gmail.com>
2024-01-04 14:10:23 -0500

    Update linked_list_values.py

--- linked_list_values/linked_list_values.py
+++ linked_list_values/linked_list_values.py
@@ -1,27 +1,15 @@
 
-​
-​
-​
-​
-​
-​
-​
-​
-​
-​
-​
-​
 # recursive
-# def linked_list_values(head):
-#   values = []
-#   fill_values(head, values)
-#   return values
+def linked_list_values(head):
+  values = []
+  fill_values(head, values)
+  return values
   
-# def fill_values(head, values):
-#   if head is None: 
-#     return 
-#   values.append(head.val) 
-#   fill_values(head.next, values)
+def fill_values(head, values):
+  if head is None: 
+    return 
+  values.append(head.val) 
+  fill_values(head.next, values)
   
 # iterative
 # def linked_list_values(head):
