8bd3dd379d0955b75f26ac48a2660831e96fc776
Jared Cazel <jjcazel@gmail.com>
2024-01-06 17:23:14 -0500

    Solved 'linked list find' (.py)

--- linked_list_find/linked_list_find.py
+++ linked_list_find/linked_list_find.py
@@ -4,50 +4,35 @@
 #     self.next = None
 ​
 # O(n) time and O(n) space
-def linked_list_find(head, target):
-  if head is None: 
-    return False
-  
-  if head.val == target:
-    return True
-  
-  return linked_list_find(head.next, target)
-    
-​
-​
-​
-​
-​
-​
-​
-​
-​
-​
-​
-​
-​
-​
-​
-​
-​
-​
-  
-#O(n) time and O(n) space
 # def linked_list_find(head, target):
-#   if head is None:
+#   if head is None: 
 #     return False
+  
 #   if head.val == target:
 #     return True
-#   return linked_list_find(head.next, target)
-​
-​
-​
-#O(n) time and O(1) space
-# def linked_list_find(head, target):
   
-#   while head is not None: 
-#     if head.val == target:
-#       return True
-#     head = head.next
+#   return linked_list_find(head.next, target)
     
-#   return False
+# O(n) time and O(n) space  
+def linked_list_find(head, target):
+    while head is not None:
+      if head.val == target:
+        return True
+      head = head.next
+      
+    return False
+​
+​
+​
+​
+​
+​
+​
+​
+​
+​
+​
+​
+​
+​
+​
