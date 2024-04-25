b96864ef76dc37df95a05297c57f089fed223d7c
Jared Cazel <jjcazel@gmail.com>
2024-04-24 21:59:28 -0400

    Solved 'bottom right value' (.js)

---
+++ bottom_right_value/bottom_right_value.js
@@ -0,0 +1,30 @@
+// class Node {
+//   constructor(val) {
+//     this.val = val;
+//     this.left = null;
+//     this.right = null;
+//   }
+// }
+
+
+const bottomRightValue = (root) => {
+  const queue = [ root ];
+  let lastValue = root.val; // -4
+	
+  while (queue.length) {
+    const currNode = queue.shift(); //assume this is an O(1) operation
+    lastValue = currNode.val;
+    if (currNode.left) queue.push(currNode.left);
+    if (currNode.right) queue.push(currNode.right);
+  }
+  
+  return lastValue;
+
+
+};
+
+
+module.exports = {
+  bottomRightValue,
+};
+
