30304c9617014e348a816b1c145e6a6999436fae
Jared Cazel <jjcazel@gmail.com>
2024-04-26 18:31:49 -0400

    Solved 'has path' (.js)

---
+++ has_path/has_path.js
@@ -0,0 +1,25 @@
+
+
+//O(n) time and O(n) space where n is the number of nodes
+const hasPath = (graph, src, dst) => {
+  const stack = [ src ];
+
+
+  while (stack.length) {
+    const currentNode = stack.pop();
+    console.log(currentNode);
+    if (currentNode === dst) return true;
+    for (const neighbor of graph[currentNode]) {
+      stack.push(neighbor);
+    }
+  }
+
+
+  return false;
+};
+
+
+module.exports = {
+  hasPath,
+};
+
