dd840ff21a3362021d7efe1b51e65d38ed2e36ac
Jared Cazel <jjcazel@gmail.com>
2024-04-28 09:30:20 -0400

    Update connected_components_count.js

--- connected_components_count/connected_components_count.js
+++ connected_components_count/connected_components_count.js
@@ -3,11 +3,9 @@ const connectedComponentsCount = (graph) => {
   const visited = new Set();
   let compCount = 0;
 
-
   for (const node in graph) {
     if (exploreComponent(graph, node, visited)) compCount++;;
   }
-
 
   return compCount;
 };
@@ -17,11 +15,9 @@ const exploreComponent = (graph, node, visited) => {
   if (visited.has(String(node))) return false;
   visited.add(String(node));
 
-
   for (const neighbor of graph[node]) {
     exploreComponent(graph, neighbor, visited);
   }
-
 
   return true;
 }
