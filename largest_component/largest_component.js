266d6b88e5ef70d2dc7aec4fe02219e98cbd2fbb
Jared Cazel <jjcazel@gmail.com>
2024-05-02 14:46:09 -0400

    Solved 'largest component' (.js)

--- largest_component/largest_component.js
+++ largest_component/largest_component.js
@@ -1,96 +1,29 @@
-// O(e) time and O(n) space where n is the num of nodes nad e is the num of edges
+// O(e) time and O(n) where e is the num of edges and n is the num of nodes
 const largestComponent = (graph) => {
+  const visited = new Set();
   let maxCount = 0;
-  let visited = new Set();
   for (const node in graph) {
-    let currCount = exploreSize(graph, node, visited);
-    if (currCount > maxCount) maxCount = currCount
+    let currCount = countPath(graph, node, visited);
+    if (currCount > maxCount) maxCount = currCount;
   }
-
+  
   return maxCount;
 };
 
-const exploreSize = (graph, node, visited) => {
+
+const countPath = (graph, node, visited) => {
   if (visited.has(String(node))) return 0;
   visited.add(String(node));
 
-  let sum = 1;
+
+  let currCount = 1;
   for (const neighbor of graph[node]) {
-    sum += exploreSize(graph, neighbor, visited);
+    currCount += countPath(graph, neighbor, visited);
   }
 
-  return sum;
+
+  return currCount;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const largestComponent = (graph) => {
-//   const visited = new Set();
-//   let maxLength = 0;
-  
-//   for (let node in graph) {
-//     const size = exploreSize(graph, node, visited);
-//     if (size > maxLength) maxLength = size;
-//   }
-  
-//   return maxLength;
-// };
-
-
-// const exploreSize = (graph, node, visited) => {
-//   if (visited.has(String(node))) return 0
-  
-//   visited.add(String(node));
-  
-//   let size = 1;
-//   for (let neighbor of graph[node]) {
-//     size += exploreSize(graph, neighbor, visited)
-//   }
-  
-//   return size;
-// }
 
 
 module.exports = {
