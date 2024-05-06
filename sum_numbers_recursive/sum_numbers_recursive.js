97f7e2f1304187f96d7142113e594825790880ee
Jared Cazel <jjcazel@gmail.com>
2024-05-06 15:30:55 -0400

    Solved 'sum numbers recursive' (.js)

--- sum_numbers_recursive/sum_numbers_recursive.js
+++ sum_numbers_recursive/sum_numbers_recursive.js
@@ -6,15 +6,14 @@ sumNumbersRecursive([700, 70, 7]); // -> 777
 sumNumbersRecursive([-10, -9, -8, -7, -6, -5, -4, -3, -2, -1]); // -> -55
 sumNumbersRecursive([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]); // -> 0
 sumNumbersRecursive([123456789, 12345678, 1234567, 123456, 12345, 1234, 123, 12, 1, 0]); // -> 1836311903
+
+
 // O(n) time and O(n) space n is the length of numbers
 const sumNumbersRecursive = (numbers, i = 0) => {
-  if (!numbers.length) return 0;
-  if (i === numbers.length - 1) return numbers[i];
+  if (i === numbers.length) return 0;
 
 
-  let sum = numbers[i];
-  sum += sumNumbersRecursive(numbers, i + 1);
-  return sum;
+  return numbers[i] + sumNumbersRecursive(numbers, i + 1);
 };
 
 
