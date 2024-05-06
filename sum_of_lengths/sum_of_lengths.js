96ceaff09b2a08d30032be5ef03220e126a588e8
Jared Cazel <jjcazel@gmail.com>
2024-05-06 15:48:50 -0400

    Solved 'sum of lengths' (.js)

--- sum_of_lengths/sum_of_lengths.js
+++ sum_of_lengths/sum_of_lengths.js
@@ -3,10 +3,10 @@ sumOfLengths(["bike", "at", "pencils", "phone"]); // -> 18
 sumOfLengths([]); // -> 0
 sumOfLengths(["", " ", "  ", "   ", "    ", "     "]); // -> 15
 sumOfLengths(["0", "313", "1234567890"]); // -> 14 
-//O(n^2) time and O(n^2) space
+//O(n) time and O(n) space
 const sumOfLengths = (strings, i = 0) => {
   if (strings.length === i) return 0;
-  return strings[0].length + sumOfLengths(strings.slice(1));
+  return strings[i].length + sumOfLengths(strings, i + 1);
 };
 
 
