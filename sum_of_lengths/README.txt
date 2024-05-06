41700e693d9b8bed4f957fe0d930b51bb6a65347
Jared Cazel <jjcazel@gmail.com>
2024-05-06 15:47:41 -0400

    Solved 'sum of lengths' (.js)

---
+++ sum_of_lengths/README.txt
@@ -0,0 +1,23 @@
+Write a function sumOfLengths that takes in array of strings and returns the total length of the strings.
+
+You must solve this recursively.
+
+test_00
+
+sumOfLengths(["goat", "cat", "purple"]); // -> 13
+
+test_01
+
+sumOfLengths(["bike", "at", "pencils", "phone"]); // -> 18
+
+test_02
+
+sumOfLengths([]); // -> 0
+
+test_03
+
+sumOfLengths(["", " ", "  ", "   ", "    ", "     "]); // -> 15
+
+test_04
+
+sumOfLengths(["0", "313", "1234567890"]); // -> 14 
---
+++ sum_of_lengths/sum_of_lengths.js
@@ -0,0 +1,16 @@
+sumOfLengths(["goat", "cat", "purple"]); // -> 13
+sumOfLengths(["bike", "at", "pencils", "phone"]); // -> 18
+sumOfLengths([]); // -> 0
+sumOfLengths(["", " ", "  ", "   ", "    ", "     "]); // -> 15
+sumOfLengths(["0", "313", "1234567890"]); // -> 14 
+//O(n^2) time and O(n^2) space
+const sumOfLengths = (strings) => {
+  if (strings.length === 0) return 0;
+  return strings[0].length + sumOfLengths(strings.slice(1));
+};
+
+
+module.exports = {
+  sumOfLengths,
+};
+
