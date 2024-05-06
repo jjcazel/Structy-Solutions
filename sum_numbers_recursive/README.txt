7927e569eb3d5245cf0ecbde216e8f87f3185ad9
Jared Cazel <jjcazel@gmail.com>
2024-05-06 15:15:52 -0400

    Solved 'sum numbers recursive' (.js)

---
+++ sum_numbers_recursive/README.txt
@@ -0,0 +1,35 @@
+Watch the Approach video first!
+
+Write a function sumNumbersRecursive that takes in an array of numbers and returns the sum of all the numbers in the array. All elements will be integers. Solve this recursively.
+
+test_00
+
+sumNumbersRecursive([5, 2, 9, 10]); // -> 26
+
+test_01
+
+sumNumbersRecursive([1, -1, 1, -1, 1, -1, 1]); // -> 1
+
+test_02
+
+sumNumbersRecursive([]); // -> 0
+
+test_03
+
+sumNumbersRecursive([1000, 0, 0, 0, 0, 0, 1]); // -> 1001
+
+test_04
+
+sumNumbersRecursive([700, 70, 7]); // -> 777
+
+test_05
+
+sumNumbersRecursive([-10, -9, -8, -7, -6, -5, -4, -3, -2, -1]); // -> -55
+
+test_06
+
+sumNumbersRecursive([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]); // -> 0
+
+test_07
+
+sumNumbersRecursive([123456789, 12345678, 1234567, 123456, 12345, 1234, 123, 12, 1, 0]); // -> 1836311903
---
+++ sum_numbers_recursive/sum_numbers_recursive.js
@@ -0,0 +1,24 @@
+sumNumbersRecursive([5, 2, 9, 10]); // -> 26
+sumNumbersRecursive([1, -1, 1, -1, 1, -1, 1]); // -> 1
+sumNumbersRecursive([]); // -> 0
+sumNumbersRecursive([1000, 0, 0, 0, 0, 0, 1]); // -> 1001
+sumNumbersRecursive([700, 70, 7]); // -> 777
+sumNumbersRecursive([-10, -9, -8, -7, -6, -5, -4, -3, -2, -1]); // -> -55
+sumNumbersRecursive([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]); // -> 0
+sumNumbersRecursive([123456789, 12345678, 1234567, 123456, 12345, 1234, 123, 12, 1, 0]); // -> 1836311903
+// O(n) time and O(n) space n is the length of numbers
+const sumNumbersRecursive = (numbers, i = 0) => {
+  if (!numbers.length) return 0;
+  if (i === numbers.length - 1) return numbers[i];
+
+
+  let sum = numbers[i];
+  sum += sumNumbersRecursive(numbers, i + 1);
+  return sum;
+};
+
+
+module.exports = {
+  sumNumbersRecursive,
+};
+
