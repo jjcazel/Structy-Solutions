00d4c5ab08ab82ba261d2feb7c1fc461000cf614
Jared Cazel <jjcazel@gmail.com>
2024-05-08 16:25:40 -0400

    Update reverse_list.js

--- reverse_list/reverse_list.js
+++ reverse_list/reverse_list.js
@@ -1,40 +1,4 @@
-const a = new Node("a");
-const b = new Node("b");
-const c = new Node("c");
-const d = new Node("d");
-const e = new Node("e");
-const f = new Node("f");
 
-
-a.next = b;
-b.next = c;
-c.next = d;
-d.next = e;
-e.next = f;
-
-
-// a -> b -> c -> d -> e -> f
-
-
-reverseList(a); // f -> e -> d -> c -> b -> a
-const x = new Node("x");
-const y = new Node("y");
-
-
-x.next = y;
-
-
-// x -> y
-
-
-reverseList(x); // y -> x
-const p = new Node("p");
-
-
-// p
-
-
-reverseList(p); // p
 // class Node {
 //   constructor(val) {
 //     this.val = val;
