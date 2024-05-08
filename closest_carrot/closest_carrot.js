29f0ba9f1a04e041a745e3e1aa1f3d40135566de
Jared Cazel <jjcazel@gmail.com>
2024-05-08 09:47:56 -0400

    Update closest_carrot.js

--- closest_carrot/closest_carrot.js
+++ closest_carrot/closest_carrot.js
@@ -1,79 +1,4 @@
-const grid = [
-  ['O', 'O', 'O', 'O', 'O'],
-  ['O', 'X', 'O', 'O', 'O'],
-  ['O', 'X', 'X', 'O', 'O'],
-  ['O', 'X', 'C', 'O', 'O'],
-  ['O', 'X', 'X', 'O', 'O'],
-  ['C', 'O', 'O', 'O', 'O'],
-];
 
-
-closestCarrot(grid, 1, 2); // -> 4
-const grid = [
-  ['O', 'O', 'O', 'O', 'O'],
-  ['O', 'X', 'O', 'O', 'O'],
-  ['O', 'X', 'X', 'O', 'O'],
-  ['O', 'X', 'C', 'O', 'O'],
-  ['O', 'X', 'X', 'O', 'O'],
-  ['C', 'O', 'O', 'O', 'O'],
-];
-
-
-closestCarrot(grid, 0, 0); // -> 5
-const grid = [
-  ['O', 'O', 'X', 'X', 'X'],
-  ['O', 'X', 'X', 'X', 'C'],
-  ['O', 'X', 'O', 'X', 'X'],
-  ['O', 'O', 'O', 'O', 'O'],
-  ['O', 'X', 'X', 'X', 'X'],
-  ['O', 'O', 'O', 'O', 'O'],
-  ['O', 'O', 'C', 'O', 'O'],
-  ['O', 'O', 'O', 'O', 'O'],
-];
-
-
-closestCarrot(grid, 3, 4); // -> 9
-const grid = [
-  ['O', 'O', 'X', 'O', 'O'],
-  ['O', 'X', 'X', 'X', 'O'],
-  ['O', 'X', 'C', 'C', 'O'],
-];
-
-
-closestCarrot(grid, 1, 4); // -> 2
-const grid = [
-  ['O', 'O', 'X', 'O', 'O'],
-  ['O', 'X', 'X', 'X', 'O'],
-  ['O', 'X', 'C', 'C', 'O'],
-];
-
-
-closestCarrot(grid, 2, 0); // -> -1
-const grid = [
-  ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
-  ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
-  ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
-  ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
-  ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
-  ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
-  ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
-  ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
-  ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
-  ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
-  ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'X', 'X'],
-  ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'X', 'C'],
-];
-
-
-closestCarrot(grid, 0, 0); // -> -1
-const grid = [
-  ['O', 'O', 'X', 'C', 'O'],
-  ['O', 'X', 'X', 'X', 'O'],
-  ['C', 'X', 'O', 'O', 'O'],
-];
-
-
-closestCarrot(grid, 2, 2); // -> 5
 //O(n) time and O(n) space
 const closestCarrot = (grid, startRow, startCol) => {
   const visited = new Set();
@@ -113,17 +38,9 @@ const closestCarrot = (grid, startRow, startCol) => {
 };
 
 
-const grid = [
-  ['O', 'O', 'O', 'O', 'O'],
-  ['O', 'X', 'O', 'O', 'O'],
-  ['O', 'X', 'X', 'O', 'O'],
-  ['O', 'X', 'C', 'O', 'O'],
-  ['O', 'X', 'X', 'O', 'O'],
-  ['C', 'O', 'O', 'O', 'O'],
-];
 
 
-closestCarrot(grid, 1, 2);
+
 
 
 module.exports = {
