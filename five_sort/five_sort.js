59315f08fe1217f90ec22711a7d992ad03a2f753
Jared Cazel <jjcazel@gmail.com>
2024-05-08 15:17:02 -0400

    Update five_sort.js

--- five_sort/five_sort.js
+++ five_sort/five_sort.js
@@ -1,20 +1,5 @@
-fiveSort([12, 5, 1, 5, 12, 7]);
-// -> [12, 7, 1, 12, 5, 5] 
-fiveSort([5, 2, 5, 6, 5, 1, 10, 2, 5, 5]);
-// -> [2, 2, 10, 6, 1, 5, 5, 5, 5, 5] 
-fiveSort([5, 5, 5, 1, 1, 1, 4]);
-// -> [4, 1, 1, 1, 5, 5, 5] 
-fiveSort([5, 5, 6, 5, 5, 5, 5]);
-// -> [6, 5, 5, 5, 5, 5, 5] 
-fiveSort([5, 1, 2, 5, 5, 3, 2, 5, 1, 5, 5, 5, 4, 5]);
-// -> [4, 1, 2, 1, 2, 3, 5, 5, 5, 5, 5, 5, 5, 5] 
-const fives = new Array(20000).fill(5);
-const fours = new Array(20000).fill(4);
-const nums = [...fives, ...fours];
-fiveSort(nums);
-// twenty-thousand 4s followed by twenty-thousand 5s
-// -> [4, 4, 4, 4, ..., 5, 5, 5, 5]
-//O(n) time and O(n) space where n is the length of nums 
+
+//O(n) time and O(1) space where n is the length of nums 
 const fiveSort = (nums) => {
   let startIdx = 0, endIdx = nums.length - 1;
 
