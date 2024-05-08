e0aebffe3ba2795ff6ef8c0030293a7e11f8c292
Jared Cazel <jjcazel@gmail.com>
2024-05-08 14:51:49 -0400

    Update intersection.js

--- intersection/intersection.js
+++ intersection/intersection.js
@@ -1,14 +1,4 @@
-intersection([4,2,1,6], [3,6,9,2,10]) // -> [2,6]
-intersection([2,4,6], [4,2]) // -> [2,4]
-intersection([4,2,1], [1,2,4,6]) // -> [1,2,4]
-intersection([0,1,2], [10,11]) // -> []
-const a = [];
-const b = [];
-for (let i = 0; i < 50000; i += 1) {
-  a.push(i);
-  b.push(i);
-}
-intersection(a, b) // -> [0,1,2,3,..., 49999]
+
 //O(n + m) time and O(min(n, m)) space where n is the array a and m is the length of array b
 const intersection = (a, b) => {
   const arrayASet = new Set(a);
