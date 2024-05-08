76a3583600ebe89a45cb8ea54eb2356f8d2e8474
Jared Cazel <jjcazel@gmail.com>
2024-05-08 15:47:08 -0400

    Update linked_list_values.js

--- linked_list_values/linked_list_values.js
+++ linked_list_values/linked_list_values.js
@@ -1,38 +1,4 @@
-const a = new Node("a");
-const b = new Node("b");
-const c = new Node("c");
-const d = new Node("d");
 
-
-a.next = b;
-b.next = c;
-c.next = d;
-
-
-// a -> b -> c -> d
-
-
-linkedListValues(a); // -> [ 'a', 'b', 'c', 'd' ]
-const x = new Node("x");
-const y = new Node("y");
-
-
-x.next = y;
-
-
-// x -> y
-
-
-linkedListValues(x); // -> [ 'x', 'y' ]
-const q = new Node("q");
-
-
-// q
-
-
-linkedListValues(q); // -> [ 'q' ]
-linkedListValues(null); // -> [ ]
-// class Node {
 //   constructor(val) {
 //     this.val = val;
 //     this.next = null;
