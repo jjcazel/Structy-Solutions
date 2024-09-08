922c23c9ce87d74fd916c60fb86130d0d21de112
Jared Cazel <jjcazel@gmail.com>
2024-09-08 09:20:37 -0400

    Solved 'compress' (.js)

--- compress/compress.js
+++ compress/compress.js
@@ -1,37 +1,30 @@
-'aaa' compresses to '3a'
-'cc' compresses to '2c'
-'t' should remain as 't'
-compress('ccaaatsss'); // -> '2c3at3s'
-compress('ssssbbz'); // -> '4s2bz'
-compress('ppoppppp'); // -> '2po5p'
-compress('nnneeeeeeeeeeeezz'); // -> '3n12e2z'
-compress('yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy'); 
-// -> '127y'
-//O(n) time and O(n) space where n is the length of the input string
 const compress = (s) => {
-  const compressed = [];
-  let i = 0; // 2
-  let j = 0;
+  const nums = '123456789';
+  const compressedStr = []; // []
+  let i = 0; // 0
+  let j = 0; // 0
+
+
+  let count = 1; // 1
   while (j <= s.length) {
-    if (s[i] === s[j]) {
+    const currChar = s[j]; // 'c'
+    if (currChar === s[i]) {
+      if (i !== j) {
+        count++;
+      }
       j++;
     } else {
-      let count = j - i;
-      if (count > 1) {
-        compressed.push(count);
-        compressed.push(s[i]);
-      } else {
-        compressed.push(s[i]);
-      }
+      compressedStr.push(count > 1 ? count : '');
+      compressedStr.push(s[i]);
       i = j;
-    } 
+      j++;
+      count = 1;
+    }
   }
 
 
-  return compressed.join('');
+  return compressedStr.join('');
 };
-
-
 
 
 module.exports = {
