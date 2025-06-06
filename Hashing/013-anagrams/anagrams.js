c7512f5d5111b78ad5fad112e39dd8fb5b5a56e8
Jared Cazel <jjcazel@gmail.com>
2025-06-06 06:57:26 -0400

    Solved 'anagrams' (node)

---
+++ Hashing/013-anagrams/anagrams.js
@@ -0,0 +1,24 @@
+// O(n) time and O(n) space
+const anagrams = (s1, s2) => {
+  if (s1.length !== s2.length) return false;
+  
+  const seenChars = {}; // {'t':}
+  for (let char of s1) {
+    if (!seenChars[char]) {
+      seenChars[char] = 0; 
+    }
+    seenChars[char]++;
+  }
+
+  for (let char2 of s2) {
+    if (!seenChars[char2] || seenChars[char2] === 0) return false;
+    seenChars[char2]--;
+  }
+
+  return true;
+};
+
+
+module.exports = {
+  anagrams,
+};
