a1f10334a9137a4c628c6ae0e2d50f6f4ac56862
Jared Cazel <jjcazel@gmail.com>
2025-06-09 06:33:34 -0400

    Solved 'tree min value' (node)

--- Binary-Tree-I/057-tree-min-value/tree-min-value.js
+++ Binary-Tree-I/057-tree-min-value/tree-min-value.js
@@ -7,18 +7,24 @@
 // }
 
 //O(n) time and O(n) space - look at every node and store them on the stack
+// const treeMinValue = (root) => {
+//   const stack = [ root ];
+//   let minValue = Infinity;
+
+//   while (stack.length) {
+//     const currNode = stack.pop();
+//     if (currNode.val < minValue) minValue = currNode.val;
+//     if (currNode.left) stack.push(currNode.left);
+//     if (currNode.right) stack.push(currNode.right);
+//   }
+
+//   return minValue;
+// };
+
+// O(n) time and O(n) space
 const treeMinValue = (root) => {
-  const stack = [ root ];
-  let minValue = Infinity;
-
-  while (stack.length) {
-    const currNode = stack.pop();
-    if (currNode.val < minValue) minValue = currNode.val;
-    if (currNode.left) stack.push(currNode.left);
-    if (currNode.right) stack.push(currNode.right);
-  }
-
-  return minValue;
+  if (!root) return Infinity;
+  return Math.min(root.val, treeMinValue(root.left), treeMinValue(root.right));
 };
 
 module.exports = {
