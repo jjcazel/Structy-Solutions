c1651d239f6dcc64eeca42d777b9a81cc5784d1c
Jared Cazel <jjcazel@gmail.com>
2025-09-28 17:26:21 -0400

    Solved 'undirected path' (node)

--- Graph-I/084-undirected-path/undirected-path.js
+++ Graph-I/084-undirected-path/undirected-path.js
@@ -1,43 +1,33 @@
-// O(e) time and O(n) space where e is the number of edges and n is the number of nodes on the stack
 const undirectedPath = (edges, nodeA, nodeB) => {
-  const graph = makeGraph(edges); 
+  const graph = buildGraph(edges);
+
   return hasPath(graph, nodeA, nodeB, new Set());
 };
 
 function hasPath(graph, src, dst, visited) {
-  if (src === dst) return true;
-  if (visited.has(src)) return false;
+  if (visited.has(src)) return false; // {'j', 'i', 'k', }
   visited.add(src);
-  for (const neighbor of graph[src]) {
-    if (hasPath(graph, neighbor, dst, visited)) return true;
+
+  if (src === dst) return true;
+
+  for (let neighbor of graph[src]) {
+    if (hasPath(graph, neighbor, dst, visited) === true) return true;
   }
 
   return false;
 }
 
-function makeGraph(edges) {
+function buildGraph(edges) {
   const graph = {};
-  for (const edge of edges) {
-    const [ nodeA, nodeB ] = edge;
-    if (!(nodeA in graph)) graph[nodeA] = [];
-    if (!(nodeB in graph)) graph[nodeB] = [];
+  for (const [ nodeA, nodeB ] of edges) {
+    if(!(nodeA in graph)) graph[nodeA] = [];
+    if(!(nodeB in graph)) graph[nodeB] = [];
     graph[nodeA].push(nodeB);
     graph[nodeB].push(nodeA);
   }
 
   return graph;
 }
-
-// const edges = [
-//   ['i', 'j'],
-//   ['k', 'i'],
-//   ['m', 'k'],
-//   ['k', 'l'],
-//   ['o', 'n']
-// ];
-
-// console.log(makeGraph(edges));
-
 
 module.exports = {
   undirectedPath,
