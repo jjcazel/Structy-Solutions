6cbf7f13143a9a4ee15b9f7310897d8b9f60111d
Jared Cazel <jjcazel@gmail.com>
2025-09-29 07:56:31 -0400

    Solved 'max value' (node)

--- Introduction/004-max-value/max-value.js
+++ Introduction/004-max-value/max-value.js
@@ -1,10 +1,8 @@
-// O(n) time and O(1) space, worst case O(n)
+//O(n) time and O(1) space where n is the length of nums
 const maxValue = (nums) => {
-  let maxNum = -Infinity;
+  let maxNum = nums[0];
   for (let num of nums) {
-    if (num > maxNum) {
-      maxNum = num;
-    }
+    if (num > maxNum) maxNum = num;
   }
 
   return maxNum;
