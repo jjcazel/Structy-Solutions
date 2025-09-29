a489b7b1461224f81cad728ce5d9c99acb6178be
Jared Cazel <jjcazel@gmail.com>
2025-09-29 07:45:34 -0400

    Solved 'is prime' (node)

--- Introduction/006-is-prime/is-prime.js
+++ Introduction/006-is-prime/is-prime.js
@@ -1,61 +1,13 @@
-// O(n) time and O(1) space
+// O(sqrt(n)) time and O(1) space where n is the numbers up to the sqrt of n
 const isPrime = (n) => {
-  if (n === 1) return false;
-  if (n === 2) return true;
-  for (let num = 2; num < n; num++) {
+  if (n < 2) return false;
+  for (let num = 2; num <= Math.sqrt(n); num++) {
     if (n % num === 0) return false;
   }
 
   return true;
 };
 
-
-
-console.log(isPrime(7919)); //true
-console.log(isPrime(7918)); //false
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const isPrime = (n) => {
-//    if (n < 2) return false;
-  
-//   for (let num = 2; num <= Math.sqrt(n); num++) {
-//     if (n % num === 0) return false;
-//   } 
-
-//   return true;
-// };
-
-
-
-// const isPrime = (n) => {
-//   if (n < 2) return false;
-  
-//   for (let num = 2; num < n; num++) {
-//     if (n % num === 0) return false;
-//   } 
-
-//   return true;
-// };
-
-
 module.exports = {
-  isPrime
+  isPrime,
 };
