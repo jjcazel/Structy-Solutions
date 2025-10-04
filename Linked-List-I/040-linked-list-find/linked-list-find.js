0eeb3cb10bbb1e09d15327c39a07ab549b4d59fc
Jared Cazel <jjcazel@gmail.com>
2025-10-03 22:48:58 -0400

    Solved 'linked list find' (node)

---
+++ Linked-List-I/040-linked-list-find/linked-list-find.js
@@ -0,0 +1,37 @@
+// class Node {
+//   constructor(val) {
+//     this.val = val;
+//     this.next = null;
+//   }
+// }
+
+//O(n) time and O(1) space
+const linkedListFind = (head, target) => {
+  let current = head;
+
+  while (current !== null) {
+    if (current.val === target) return true;
+    current = current.next;
+  }
+
+  return false;
+};
+
+module.exports = {
+  linkedListFind,
+};
+
+// Write a function, linkedListFind, that takes in the head of a linked list and a target value. The function should return a boolean indicating whether or not the linked list contains the target.
+
+// const a = new Node("a");
+// const b = new Node("b");
+// const c = new Node("c");
+// const d = new Node("d");
+
+// a.next = b;
+// b.next = c;
+// c.next = d;
+
+// a -> b -> c -> d
+
+// linkedListFind(a, "q"); // true
