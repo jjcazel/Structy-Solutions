7b7e194e1d22af8c43dac24154fb9049ebfc437b
Jared Cazel <jjcazel@gmail.com>
2025-10-09 21:33:33 -0400

    Solved 'reverse list' (node)

--- Linked-List-I/042-reverse-list/reverse-list.js
+++ Linked-List-I/042-reverse-list/reverse-list.js
@@ -5,16 +5,16 @@
 //   }
 // }
 
-//O(n) time and O(1) space
+//O(n) time and O(1) space where n is the num of nodes
 const reverseList = (head) => {
   let prev = null;
   let current = head;
 
   while (current !== null) {
-    let tempNext = current.next; // null
-    current.next = prev; // e
-    prev = current; // f
-    current = tempNext; // null
+    let tempNext = current.next;
+    current.next = prev;
+    prev = current;
+    current = tempNext;
   }
 
   return prev;
@@ -26,28 +26,10 @@ module.exports = {
 
 // Write a function, reverseList, that takes in the head of a linked list as an argument. The function should reverse the order of the nodes in the linked list in-place and return the new head of the reversed linked list.
 
-// const a = new Node("a");
-// const b = new Node("b");
-// const c = new Node("c");
-// const d = new Node("d");
-// const e = new Node("e");
-// const f = new Node("f");
+// f -> e -> d -> c -> b -> a -> null
 
-// a.next = b;
-// b.next = c;
-// c.next = d;
-// d.next = e;
-// e.next = f;
+// null ->  a  ->  b  ->  c  ->  d  ->  e  ->  f  ->  null
+                                            //  prev   curr  temp
 
-// null -> a -> b -> c -> d -> e -> f -> null
-//                                 prev curr next 
 
-//  f -> e -> d -> c -> b -> a -> null
-
-// reverseList(a); // f -> e -> d -> c -> b -> a 
-
-// prev = null
-// temp next
-// replace current node's next with prev
-// thn proceed with iteration by setting the current node to the temp next (which was the old next before resetting it)
-// prev = 
+// f -> e -> d -> c -> b -> a -> null 
