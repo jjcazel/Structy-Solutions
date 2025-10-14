e73f0beac5c105840732eb2e161a1f679f9700fe
Jared Cazel <jjcazel@gmail.com>
2025-10-13 21:54:58 -0400

    Solved 'zipper lists' (node)

--- Linked-List-I/043-zipper-lists/zipper-lists.js
+++ Linked-List-I/043-zipper-lists/zipper-lists.js
@@ -1,15 +1,44 @@
+// class Node {
+//   constructor(val) {
+//     this.val = val;
+//     this.next = null;
+//   }
+// }
+
+// O(n) time and O(1) space where n is the number of nodes
 const zipperLists = (head1, head2) => {
-  if (head1 === null && head2 === null) return null;
-  if (head1 === null) return head2;
-  if (head2 === null) return head1;
-  
-  const next1 = head1.next;
-  const next2 = head2.next;
-  head1.next = head2;
-  head2.next = zipperLists(next1, next2);
+  let curr1 = head1.next;
+  let curr2 = head2;
+  let tail = head1;
+  let count = 0;
+
+  while (curr1 !== null && curr2 !== null) {
+    if (count % 2 === 0) {
+      tail.next = curr2;
+      curr2 = curr2.next;
+    } else {
+      tail.next = curr1;
+      curr1 = curr1.next;
+    }
+    tail = tail.next;
+    count += 1;
+  }
+
+  if (curr1) tail.next = curr1;
+  if (curr2) tail.next = curr2;
+
   return head1;
 };
 
 module.exports = {
-    zipperLists,
+  zipperLists,
 };
+
+//  a  ->  b  ->  c
+                curr1
+//  x  ->  y 
+             curr2
+//  a  ->  x -> b -> y -> c
+                    tail
+
+// count = 3: even is curr2 and odd is curr1
