25784da6598990e67b65602a64cead7e3ef6f6e0
Jared Cazel <jjcazel@gmail.com>
2025-10-14 22:21:18 -0400

    Solved 'reverse list' (node)

--- Linked-List-I/042-reverse-list/reverse-list.js
+++ Linked-List-I/042-reverse-list/reverse-list.js
@@ -5,12 +5,13 @@
 //   }
 // }
 
-//O(n) time and O(n) space where n is the num of nodes
+//O(n) time and O(n) space
 const reverseList = (head, prev = null) => {
   if (head === null) return prev;
-  const tempNext = head.next; // b
-  head.next = prev; 
-  return reverseList(tempNext, head)
+  const tempNext = head.next;
+  head.next = prev;
+  prev = head;
+  return reverseList(tempNext, head);
 };
 
 module.exports = {
@@ -19,8 +20,9 @@ module.exports = {
 
 // Write a function, reverseList, that takes in the head of a linked list as an argument. The function should reverse the order of the nodes in the linked list in-place and return the new head of the reversed linked list.
 
-//  a  ->  b  ->  c 
+// null -> a -> b -> c -> d -> e -> f
+//                                 p    h    n
+// null -> f -> e -> d -> c -> b -> a -> null
 
-// c -> b -> a
 
-// a -> null
+// f -> e -> d -> c -> b -> a
