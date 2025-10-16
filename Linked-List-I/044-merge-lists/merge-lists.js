331d4b8cafe79b4e09b7ce553abdbdf29134d517
Jared Cazel <jjcazel@gmail.com>
2025-10-15 21:37:42 -0400

    Solved 'merge lists' (node)

--- Linked-List-I/044-merge-lists/merge-lists.js
+++ Linked-List-I/044-merge-lists/merge-lists.js
@@ -5,38 +5,31 @@ class Node {
   }
 }
 
-// O(n) time and O(1) space where n is the number of nodes
+// O(n + m) time and O(n + m) space where n is the number of nodes
 const mergeLists = (head1, head2) => {
-  let curr1 = head1;
-  let curr2 = head2;
-  const dummyNode = new Node(null);
-  let tail = dummyNode;
-
-  while (curr1 !== null && curr2 !== null) {
-    if (curr1.val < curr2.val) {
-      tail.next = curr1;
-      curr1 = curr1.next;
-    } else {
-      tail.next = curr2;
-      curr2 = curr2.next;
-    }
-    tail = tail.next;
+  if (head1 === null) return head2;
+  if (head2 === null) return head1;
+  if (head1.val < head2.val) {
+    const next1 = head1.next
+    head1.next = mergeLists(next1, head2);
+    return head1;
+  } else {
+    const next2 = head2.next;
+    head2.next = mergeLists(head1, next2);
+    return head2;
   }
-
-  if (curr1) tail.next = curr1;
-  if (curr2) tail.next = curr2;
-
-  return dummyNode.next;
 };
 
 module.exports = {
   mergeLists,
 };
 
-// 5 -> 7 -> 10 -> 12 -> 20 -> 28
-//                           curr1
+// 5 -> 7 -> 10 -> 12
+//     h1
 // 6 -> 8 -> 9 -> 25
-//                  curr2
+//     h2
 
-//   null ->  5 -> 6 -> 7 -> 8 -> 9 -> 10 -> 12 -> 20 -> 25 -> 28
-//dummyNode                                             tail
+//Frame 1: 5 -> mergeLists(7, 6)
+//Frame 2: 5 -> 6 -> mergeLists(7, 8)
+//Frame 3: 5 -> 6 -> 7 -> mergeLists(10, 8)
+
