cf1c07166724dd30e6d3dca15d6a1a00aa2b0350
Jared Cazel <jjcazel@gmail.com>
2025-10-16 22:25:41 -0400

    Solved 'depth first values' (node)

--- Binary-Tree-I/056-depth-first-values/depth-first-values.js
+++ Binary-Tree-I/056-depth-first-values/depth-first-values.js
@@ -6,17 +6,17 @@
 //   }
 // }
 
-// O(n) time and O(n) space where n is the number of nodes
+//O(n) time and O(n) space where n is the number of nodes
 const depthFirstValues = (root) => {
-  if(root === null) return [];
-  const stack = [ root ];
+  if (root === null) return [];
   const dfsVals = [];
+  const stack = [ root ];
 
-  while(stack.length !== 0) {
+  while (stack.length > 0) {
     const currNode = stack.pop();
     dfsVals.push(currNode.val);
-    if(currNode.right) stack.push(currNode.right);
-    if(currNode.left) stack.push(currNode.left);
+    if (currNode.right) stack.push(currNode.right);
+    if (currNode.left) stack.push(currNode.left);
   }
 
   return dfsVals;
@@ -26,7 +26,6 @@ module.exports = {
   depthFirstValues,
 };
 
-
 // Write a function, depthFirstValues, that takes in the root of a binary tree. The function should return an array containing all values of the tree in depth-first order.
 
 //      a
@@ -34,6 +33,3 @@ module.exports = {
 //   b     c
 //  / \     \
 // d   e     f
-
-
-
