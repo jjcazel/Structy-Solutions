ddb23d3bcf26aaa4c578c2e7f2dd37a3d742e3d3
Jared Cazel <jjcazel@gmail.com>
2025-10-18 10:44:42 -0400

    Solved 'tree sum' (node)

--- Binary-Tree-I/059-tree-sum/tree-sum.js
+++ Binary-Tree-I/059-tree-sum/tree-sum.js
@@ -6,17 +6,16 @@
 //   }
 // }
 
-//O(n) time and O(n) space where n is the number of nodes
 const treeSum = (root) => {
   if (root === null) return 0;
-  let sum = 0;
   const queue = [ root ];
+  let sum = 0;
 
   while (queue.length > 0) {
     const currNode = queue.shift();
     sum += currNode.val;
-    if (currNode.right) queue.push(currNode.right);
     if (currNode.left) queue.push(currNode.left);
+    if (currNode.right) queue.push(currNode.right);
   }
 
   return sum;
@@ -26,12 +25,12 @@ module.exports = {
   treeSum,
 };
 
-// Write a function, treeSum, that takes in the root of a binary tree that contains number values. The function should return the total sum of all values in the tree.
-
 //       3
 //    /    \
 //   11     4
 //  / \      \
 // 4   -2     1
 
-//treeSum(a); // -> 21
+// queue = [ 4, -2 ]
+// currNode = 4
+// sum = 18
