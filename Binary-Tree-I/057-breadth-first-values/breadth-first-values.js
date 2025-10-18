e12fecf37ea58389a55964af227bf4d96847353b
Jared Cazel <jjcazel@gmail.com>
2025-10-17 22:12:45 -0400

    Solved 'breadth first values' (node)

--- Binary-Tree-I/057-breadth-first-values/breadth-first-values.js
+++ Binary-Tree-I/057-breadth-first-values/breadth-first-values.js
@@ -6,13 +6,14 @@
 //   }
 // }
 
-// O(n) time and O(n) space where n is the number of nodes
+//O(n) time and O(n) time
 const breadthFirstValues = (root) => {
+  if (root === null) return [];
   const queue = [ root ];
   const bfsVals = [];
 
   while (queue.length > 0) {
-    const currNode = queue.shift(); // assume this is an O(1) operation
+    const currNode = queue.shift();
     bfsVals.push(currNode.val);
     if (currNode.left) queue.push(currNode.left);
     if (currNode.right) queue.push(currNode.right);
@@ -24,14 +25,3 @@ const breadthFirstValues = (root) => {
 module.exports = {
   breadthFirstValues,
 };
-
-// Write a function, breadthFirstValues, that takes in the root of a binary tree. The function should return an array containing all values of the tree in breadth-first order.
-
-//      a
-//    /   \
-//   b     c
-//  / \     \
-// d   e     f
-
-// breadthFirstValues(a); 
-//    -> ['a', 'b', 'c', 'd', 'e', 'f']
