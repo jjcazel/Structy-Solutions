50a479a3ced62a752a967c2a400fd756ce4196bd
Jared Cazel <jjcazel@gmail.com>
2025-10-22 19:37:34 -0400

    Solved 'longest streak' (node)

--- Linked-List-I/047-longest-streak/longest-streak.js
+++ Linked-List-I/047-longest-streak/longest-streak.js
@@ -5,35 +5,26 @@
 //   }
 // }
 
-//O(n) time and O(1) space
-const longestStreak = (head) => {
-  if (head === null) return 0;
-  let current = head;
-  let maxStreak = 0;
-  let currStreak = 0;
-  let currVal = head.val;
-
-  while (current !== null) {
-    if (current.val === currVal) {
-      currStreak += 1;
-    } else {
-      currStreak = 1;
-      currVal = current.val;
-    }
-    maxStreak = Math.max(currStreak, maxStreak);
-    current = current.next;
+//O(n) time and O(n) space where n is the number of nodes
+const longestStreak = (head, currVal = head.val, count = 0, maxCount = 0) => {
+  if (head === null) return Math.max(maxCount, count);
+  if (head.val === currVal) {
+    count += 1;
+    return longestStreak(head.next, currVal, count, maxCount)
+  } else {
+    maxCount = Math.max(count, maxCount);
+    count = 1;
+    currVal = head.val;
+    return longestStreak(head.next, currVal, count, maxCount)
   }
-
-  return maxStreak;
 };
 
 module.exports = {
   longestStreak,
 };
 
-// Write a function, longestStreak, that takes in the head of a linked list as an argument. The function should return the length of the longest consecutive streak of the same value within the list.
-
-// 5 -> 5 -> 7 -> 7 -> 7 -> 6
-//                            c
-// 7
-// currVal = 6, maxStreak = 3, currStreak = 1
+// 9 -> 9 -> 1 -> 9 -> 9 -> 9
+//                            h  
+// currVal = 9
+// count = 3
+// maxCount = 2
