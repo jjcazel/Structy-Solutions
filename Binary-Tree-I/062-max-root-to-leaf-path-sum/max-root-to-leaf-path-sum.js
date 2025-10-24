243c91f5f32fbb2bf89b78dace92497a3aa27326
Jared Cazel <jjcazel@gmail.com>
2025-10-23 20:42:14 -0400

    Solved 'max root to leaf path sum' (node)

--- Binary-Tree-I/062-max-root-to-leaf-path-sum/max-root-to-leaf-path-sum.js
+++ Binary-Tree-I/062-max-root-to-leaf-path-sum/max-root-to-leaf-path-sum.js
@@ -2,42 +2,25 @@
 //   constructor(val) {
 //     this.val = val;
 //     this.left = null;
-//     this.left = null;
+//     this.right = null;
 //   }
 // }
 
-// O(n) time and O(n) space where n is the number of nodes
+//O(n) time and O(n) space
 const maxPathSum = (root) => {
-  const stack = [ [root, 0] ];
-  let maxSum = -Infinity;
-
-  while (stack.length > 0) {
-    const [currNode, currSum] = stack.pop();
-    currSum += currNode.val;
-    
-    if (currNode.right) stack.push([currNode.right, currSum]);
-    if (currNode.left) stack.push([currNode.left, currSum]);
-    if (currNode.left === null && currNode.right === null) {
-      maxSum = Math.max(maxSum, currSum);
-    }
-  }
-
-  return maxSum;
+  if (root === null) return -Infinity;
+  if (root.right === null && root.left === null) return root.val;
+  return root.val + Math.max(maxPathSum(root.right), maxPathSum(root.left));
 };
 
 module.exports = {
   maxPathSum,
 };
 
-//       3
-//    /    \
-//   11     4
-//  / \      \
-// 4   -2     1
-
-// maxPathSum(a); // -> 18
-
-// stack = [  [11, 3], [4, 3], [1, 7], [4, 14], [-2, 14] ]
-//currNode = 11
-//currSum = 14
-//maxSum = 14
+//        5
+//     /    \
+//    11    54
+//  /   \
+// 20   15
+//      / \
+//     1  3
