5bfcd801ff6c89c2d7d29df936f41b27f3336626
Jared Cazel <jjcazel@gmail.com>
2025-10-27 06:39:47 -0400

    Solved 'longest streak' (node)

--- Linked-List-I/047-longest-streak/longest-streak.js
+++ Linked-List-I/047-longest-streak/longest-streak.js
@@ -5,26 +5,35 @@
 //   }
 // }
 
-//O(n) time and O(n) space where n is the number of nodes
-const longestStreak = (head, currVal = head.val, count = 0, maxCount = 0) => {
-  if (head === null) return Math.max(maxCount, count);
-  if (head.val === currVal) {
-    count += 1;
-    return longestStreak(head.next, currVal, count, maxCount)
-  } else {
-    maxCount = Math.max(count, maxCount);
-    count = 1;
-    currVal = head.val;
-    return longestStreak(head.next, currVal, count, maxCount)
+//O(n) time and O(1) space where n is the number of nodes
+const longestStreak = (head) => {
+  if (head === null) return 0;
+  let currVal = head.val;
+  let currNode = head;
+  let currStreak = 0;
+  let maxStreak = 0;
+
+  while (currNode !== null) {
+    if (currNode.val === currVal) {
+      currStreak += 1;
+    } else {
+      maxStreak = Math.max(currStreak, maxStreak);
+      currStreak = 1;
+      currVal = currNode.val
+    }
+    maxStreak = Math.max(currStreak, maxStreak);
+    currNode = currNode.next;
   }
+
+  return maxStreak;
 };
 
 module.exports = {
   longestStreak,
 };
 
-// 9 -> 9 -> 1 -> 9 -> 9 -> 9
-//                            h  
-// currVal = 9
-// count = 3
-// maxCount = 2
+// 5 -> 5 -> 7 -> 7 -> 7 -> 6
+//                            c
+// currVal = 6
+// currStreak = 1
+// maxStreak = 3
