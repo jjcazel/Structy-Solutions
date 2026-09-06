1290b132da5dbfea60a6d3e4ab7b7dbd78b13140
Jared Cazel <jjcazel@gmail.com>
2026-09-06 12:04:58 -0400

    Solved 'zipper lists' (node)

--- Linked-List-I/047-zipper-lists/zipper-lists.js
+++ Linked-List-I/047-zipper-lists/zipper-lists.js
@@ -7,22 +7,14 @@
 
 // O(n) time and O(n) space
 const zipperLists = (head1, head2) => {
-  let current1 = head1.next;
-  let current2 = head2;
-  let tail = head1;
+  if (head1 === null) return head2;
+  if (head2 === null) return head1;
 
-  while (current1 !== null && current2 !== null) {
-    tail.next = current2;
-    current2 = current2.next;
-    tail = tail.next;
-    tail.next = current1;
-    current1 = current1.next;
-    tail = tail.next;
-  }
-
-  if (current1) tail.next = current1;
-  if (current2) tail.next = current2;
-
+  let next1 = head1.next;
+  let next2 = head2.next;
+  head1.next = head2;
+  head2.next = zipperLists(next1, next2);
+  
   return head1;
 };
 
