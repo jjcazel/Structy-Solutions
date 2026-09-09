45aebcb4324d7e491b14370ff67b04a6e73d3564
Jared Cazel <jjcazel@gmail.com>
2026-09-09 15:09:29 -0400

    Solved 'remove node' (python3)

---
+++ Linked-List-I/053-remove-node/remove-node.py
@@ -0,0 +1,22 @@
+# class Node:
+#   def __init__(self, val):
+#     self.val = val
+#     self.next = None
+
+# O(n) time and O(1) space
+def remove_node(head, target_val):
+  current = head
+  prev = None
+  if head.val == target_val:
+    return head.next
+
+  while current is not None:
+    if current.val == target_val:
+      prev.next = current.next
+      return head
+
+    temp_next = current.next
+    prev = current
+    current = temp_next
+
+
