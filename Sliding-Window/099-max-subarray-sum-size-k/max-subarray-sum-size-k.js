235841ab2a6069bd6d14da55293b88e0442d9b57
Jared Cazel <jjcazel@gmail.com>
2026-09-25 12:04:57 -0400

    Solved 'max subarray sum size k' (node)

--- Sliding-Window/099-max-subarray-sum-size-k/max-subarray-sum-size-k.js
+++ Sliding-Window/099-max-subarray-sum-size-k/max-subarray-sum-size-k.js
@@ -1,13 +1,15 @@
 
-// O(n) time and O(1) space where n is the length of num
+// O(n) time and O(1) space
 const maxSubarraySumSizeK = (nums, k) => {
   let sum = 0;
+
   for (let i = 0; i < k; i++) {
     sum += nums[i];
   }
+
   let maxSum = sum;
 
-  for (let i = 0; i < nums.length - k; i++) {
+  for (let i = 0; i <= nums.length - k; i++) {
     sum += nums[i + k];
     sum -= nums[i];
     maxSum = Math.max(sum, maxSum);
@@ -15,6 +17,9 @@ const maxSubarraySumSizeK = (nums, k) => {
 
   return maxSum;
 };
+
+// maxSubarraySumSizeK([4, 2, 1, -9, 8, 4, 3], 3); // -> 15
+// // [8,4,3] is the subarray of size 3 with the maximal sum
 
 module.exports = {
   maxSubarraySumSizeK,
